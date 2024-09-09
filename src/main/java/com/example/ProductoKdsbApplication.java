836782dce12fe13e1ae97a384df1f1460a1d9fdb
KevinSilva <kevinsilva462@gmail.com>
2024-09-09 10:30:26 -0600

    Proyecto api spring boot

---
+++ src/main/java/com/example/ProductoKdsbApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoKdsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoKdsbApplication.class, args);
+	}
+
+}
